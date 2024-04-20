77c7a4e7dd96e4377704529888930f2156dee46d
Ach Rozikin:VPS-Biznet-V2 <geronimo794@gmail.com>
2024-04-20 22:38:33 +0700

    fix: Portainer stack and portainer deployment

---
+++ deploy-portainer-stack.sh
@@ -0,0 +1,25 @@
+#/bin/bash
+
+# Show all command and variable value
+set -x
+
+# Load configuration from .env file
+set -o allexport
+
+# If .env not exist then use format.env
+if [ -f .env ]; then
+	source .env
+else
+	echo "Please populate the .env file from .env.format"
+	exit
+fi
+set +o allexport
+
+# Hide all command and variable value again
+set +x
+
+source .env
+
+export PORTAINER_PORT
+
+sudo docker stack deploy -c docker-compose-portainer-stack.yaml portainer
