db9ee39cfd272a61b533775487d5ee812b5cea85
Ach Rozikin:VPS-Biznet-V2 <geronimo794@gmail.com>
2024-12-31 15:12:44 +0700

    fix: Deployment redis and make sure use fix version.

---
+++ deploy-redis.sh
@@ -0,0 +1,27 @@
+#/bin/bash
+
+# Make sure run as sudo su
+# sudo su
+
+# Show all command and variable value
+set -x
+# Load configuration from .env file
+set -o allexport
+# If .env not exist then use format.env
+if [ -f .env ]; then
+	source .env
+else
+	echo "Please populate the .env file from .env.format"
+	exit
+fi
+set +o allexport
+
+# Hide all command and variable value again
+set +x
+
+export REDIS_USERNAME
+export REDIS_PASSWORD
+export REDIS_INSIGHT_PORT
+export NETWORK
+
+docker stack deploy -c PROD-docker-compose-redis.yaml redis
